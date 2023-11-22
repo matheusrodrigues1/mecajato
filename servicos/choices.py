e43edc60bec6545e5999aa13d5b8c2e47c516819
matheusrodrigues1 <matech00@outlook.com>
2023-11-22 19:54:58 -0300

    update

--- servicos/choices.py
+++ servicos/choices.py
@@ -4,3 +4,6 @@ class ChoicesCategoriaManutencao(TextChoices):
     TROCAR_VALVULA_MOTOR = "TVM", "Trocar válcula do motor"
     TROCAR_OLEO = "TO", "Troca de óleo"
     BALANCEAMENTO = "B", "Balanceamento"
+    REVISAO_FREIOS = "RF", "Revisão dos freios"
+    LIMPEZA_AR_CONDICIONADO = "LAC", "Limpeza do sistema de ar condicionado"
+    TROCA_PASTILHAS_FREIO = "TPF", "Troca das pastilhas de freio"
